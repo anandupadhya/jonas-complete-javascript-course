a8db36a204e6af7dda9ef9bf77354d1d499ab318
Anand Upadhya <anand.upadhya@yahoo.com>
2022-08-02 08:57:01 +0800

    Regular commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUNNN!!!');
+40 + 12 + 34;
+console.log(40 + 12 + 34);
+
+console.log('Anand');
+console.log(35)
+
+let firstName = 'Anand';
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
